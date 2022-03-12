2e8cd2fb4c1da04c4645ec0bf40e7ce8ddeb1850
Will Kopil <wkopcom@gmail.com>
2022-03-12 11:16:59 +0000

    Fixed clean up

--- install-scripts/ipfs.sh
+++ install-scripts/ipfs.sh
@@ -3,7 +3,6 @@ wget https://dist.ipfs.io/go-ipfs/v0.12.0/go-ipfs_v0.12.0_linux-amd64.tar.gz
 tar -xvzf go-ipfs_v0.12.0_linux-amd64.tar.gz
 cd go-ipfs
 sudo bash install.sh
-cd ~
-echo 'this echo is working TEST'
-sudo echo 'go-ipfs' >> .gitignore
-sudo rm -f go-ipfs_v0.12.0_linux-amd64.tar.gz
+cd ..
+echo 'go-ipfs' >> .gitignore
+rm -f go-ipfs_v0.12.0_linux-amd64.tar.gz
