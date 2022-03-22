b801a21a8782e9d88965ddee6a0741c8be0e2903
Will Kopil <wkopcom@gmail.com>
2022-03-22 18:19:34 +0000

    gltf loads

--- js/app.js
+++ js/app.js
@@ -27,24 +27,61 @@ renderContainer.setAttribute('style','height:70%;width:70%;margin:auto;');
 renderContainer.appendChild( renderer.domElement );
 document.body.appendChild( renderContainer );
 
+//--------------------------------------------------------
+// ADDING A CUSTOM MODEL
+//--------------------------------------------------------
+
+const loader = new THREE.GLTFLoader();
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'assets/water.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
+
+
 // ------------------------------------------------
 // FUN STARTS HERE
 // ------------------------------------------------
 
 // Create a Cube Mesh with basic material
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: "#bb1cd4" } );
-var cube = new THREE.Mesh( geometry, material );
+//var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+//var material = new THREE.MeshBasicMaterial( { color: "#bb1cd4" } );
+//var cube = new THREE.Mesh( geometry, material );
 
 // Add cube to Scene
-scene.add( cube );
+//scene.add( cube );
 
 // Render Loop
 var render = function () {
   requestAnimationFrame( render );
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //cube.rotation.x += 0.01;
+  //cube.rotation.y += 0.01;
 
   // Render the scene
   renderer.render(scene, camera);
