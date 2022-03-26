255449f9792f70d9f8a8b2606eb87fe6ed619cc5
Will Kopil <wkopcom@gmail.com>
2022-03-26 01:47:24 +0000

    camera follows custom spline

--- js/app.js
+++ js/app.js
@@ -62,11 +62,17 @@ scene.add( light );
 
 // Camera follow spline
 var spline = new THREE.CatmullRomCurve3( [
-	new THREE.Vector3( -10, 0, 10 ),
-	new THREE.Vector3( -5, 5, 5 ),
-	new THREE.Vector3( 0, 0, 0 ),
-	new THREE.Vector3( 5, -5, 5 ),
-	new THREE.Vector3( 10, 0, 10 )
+	new THREE.Vector3(-0.41659999990463276, 1.6336999995708468, 11.282800000190726),
+	new THREE.Vector3( -1.256700000047686, 1.6338999993801129, 7.140000000238435),
+	new THREE.Vector3(-5.383714514702151, 2.322387576479296, 5.075950029095314),
+	new THREE.Vector3(-9.231299999952293, 2.166000000000003, 4.817699999570876),
+	new THREE.Vector3(-12.829999999046272, 0.5501999998092664, -1.6628000006675472),
+  new THREE.Vector3(-10.697299998998625, 0.716599999904634, -4.4946000006198705),
+  new THREE.Vector3(-5.266099998712568, 0.716599999904634, -3.0119000005721865),
+  new THREE.Vector3(4.914558664096966, 0.9011746782094197, -1.7652858079871652),
+  new THREE.Vector3(13.467731655622838, 2.83263691876875, -0.9362045858109391),
+  new THREE.Vector3(14.301267935141546, 4.084822421310777, 3.154864944851691),
+  new THREE.Vector3(11.126648373894048, 3.603639646668969, 2.8972795561863296)
 ] );
 
 //Add spline to scene
@@ -86,15 +92,13 @@ var clock = new THREE.Clock();
 
 //Debuging
 setInterval(function(){
-	console.log("X: " + camera.position.x);
+	console.log("X: " + camera.position.x + " Y: " + camera.position.y + " Z: " + camera.position.z);
 },1000);
 
 // Render Loop
 var render = function () {
   requestAnimationFrame( render );
 
-
-  /*
   // Camera follow spline
   camPosIndex++;
   if (camPosIndex > 10000) {
@@ -113,11 +117,9 @@ var render = function () {
   camera.rotation.z = camRot.z;
   
   camera.lookAt(spline.getPoint((camPosIndex+1) / 10000));
-  */
-
 
   //This line allows camera control via keys and mouse
-  controls.update(clock.getDelta());
+  //controls.update(clock.getDelta());
 
   //planet.rotation.x += 0.01;
   //planet.rotation.y += 0.01;
